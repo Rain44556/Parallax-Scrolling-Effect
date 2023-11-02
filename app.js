b4d8aebbcf48f25afa8b72abf4bd10f2de5522f7
Rain44556 <bristy44556@gmail.com>
2023-11-02 22:37:41 +0600

    title, header, shadow are added functionally in js file

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const translate =document.querySelectorAll(".translate");
+const title = document.querySelector(".title");
+const header = document.querySelector("header");
+const shadow = document.querySelector(".shadow");
+
+let header_height = header.offsetHeight;
 
 window.addEventListener('scroll',()=>{
     let scroll = window.pageYOffset;
@@ -6,4 +11,7 @@ window.addEventListener('scroll',()=>{
         let speed = element.dataset.speed;
         element.style.transform = `translateY(${scroll*speed}px)`;
     })
+
+    title.style.opacity = -scroll / (header_height/2) +1;
+    shadow.style.height = `${scroll*0.5+350}px`;
 })
