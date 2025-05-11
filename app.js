9dc770da3b74502275028260c52357408cfb9b6a
rain44556 <bristy44556@gmail.com>
2025-05-11 23:50:04 +0600

    add comment in app.js

--- app.js
+++ app.js
@@ -13,25 +13,34 @@ const bottom_line = document.querySelector(".bottom-line");
 let header_height = header.offsetHeight;
 let section_height = section.offsetHeight;
 
-window.addEventListener('scroll',()=>{
+window.addEventListener('scroll', () => {
     let scroll = window.pageYOffset;
     let sectionY = section.getBoundingClientRect();
 
 
-    translate.forEach(element =>{
+    // Parallax movement
+    translate.forEach(element => {
         let speed = element.dataset.speed;
         element.style.transform = `translateY(${scroll*speed}px)`;
     });
 
-    opacity.forEach(element =>{
+      // Opacity transition
+    opacity.forEach(element => {
     element.style.opacity = scroll/(sectionY.top + section_height);
     })
 
+     // Fade out title
     title.style.opacity = -scroll / (header_height/2) +1;
-    shadow.style.height = `${scroll*0.5+350}px`;
-    content.style.transform = `translateY(${scroll/(section_height + sectionY.top) * 50-50}px)`;
+    
+     // Shadow effect grow
+    shadow.style.height = `${scroll * 0.5 + 350}px`;
+    
+    // Content moves up
+    content.style.transform = `translateY(${scroll/(section_height + sectionY.top) * 50 - 50}px)`;
+    
+    // Image section moves down
     img_section.style.transform=`translateY(${scroll/(section_height+sectionY.top) * -50 + 50}px)`;
+     
+    // Bottom line width grow
     bottom_line.style.width = `${scroll/(sectionY.top + section_height) * 27}%`;
-
-
 })
